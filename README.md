34ec894cb42265e426ab1ad7762a99afee5c00af
Mr Sam Law <sam@chongland.com>
2021-03-30 08:09:31 +1100

    HTML blocks & Time Blocks

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # 05-HW-WorkDayPlanner
 BCS Assignment 05 - Work Day Planner
+
+## Description
+Our client is looking for a way to efficiently manage their employees work time by providing them with an app to plan out their work day hour by hour.
+
+## Criteria
+- Current day is displayed at the top of the planner
+- Timeblocks displayed for standard business hours
+- Timeblocks are color coded for past, present or future
+- Timeblock allows for event entry
+- Event saves in Timeblock & localStorage on button click
+
+## Deployment
+https://mrsamlaw.github.io/05-HW-WorkDayPlanner/
+
+## Screenshots
+
+## Psuedo Code
+- Moment.js for current day & time
+- Bootstrap Rows & columns for time block & save button
+- Moment.js for current hour & compare for color coding time blocks
+- Task Object stored in an array for time & Task [Extend with date]
+- localStorage to get/set task objects array
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+var today = moment();
+var currentDay = today.format("dddd Do MMMM YYYY");
+var currentHour = parseInt(moment().format("H"))
+// var currentHour = moment("29/03/2021", "11:05:00")
+
+$("#currentHour").text(currentHour);
+
+const timeBlock = $(".time-block");
+
+
+$(document).ready(function () {
+    $("#currentDay").text(currentDay);
+});
+// function (time) {
+//     // Function to colorise the time blocks
+//     if (moment(time).isBefore(currentHour, "hour")) {
+//         console.log("before");
+//     } else if (moment(time).isAfter(currentHour, "hour")) {
+//         console.log("after");
+//     } else if (moment(time).isSame(currentHour, "hour")) {
+//         console.log("current");
+//     }
+// }
+
+timeBlock.each(function () {
+    var parent = $(this).parents(".flex-row");
+    var timeId = parseInt(parent.attr("id"));
+    var timeString = parent.attr("id");
+    var thisHour = parseInt(moment().format("H"));
+
+    // console.log(parent);
+    // console.log(timeId);
+    console.log("timeId is: " + moment(timeId));
+    console.log("thisHour is: " + thisHour);
+
+    // console.log(moment(timeId));
+    // console.log(moment(thisHour));
+
+    if (timeId < thisHour) {
+        $(this).addClass("past").removeClass("present future");
+        console.log("before");
+    } else if (timeId > thisHour) {
+        $(this).addClass("future").removeClass("present past");
+        console.log("after");
+    } else if (timeId == thisHour) {
+        $(this).addClass("present").removeClass("past future");
+        console.log("current");
+    }
+
+    console.log(moment(timeId).isBefore(moment(thisHour), "hour"));
+    // if (moment(timeId).isBefore(thisHour, "hour")) {
+    //     $(this).addClass("past").removeClass("present future");
+    //     console.log("before");
+    // } else if (moment(timeId).isAfter(moment(thisHour), "hour")) {
+    //     $(this).addClass("future").removeClass("present past");
+    //     console.log("after");
+    // } else if (moment(timeId).isSame(moment(thisHour), "hour")) {
+    //     $(this).addClass("present").removeClass("past future");
+    //     console.log("current");
+    // }
+
+    console.log(moment(timeString).isBefore(moment("10"), "hour"));
+    console.log(moment("8").isAfter(moment("10"), "hour"));
+})
+
